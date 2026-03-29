d7b6800b3070126fef87581bf1102e1f4d546903
installer <installer@gmail.com>
2026-03-29 18:00:17 +0300

    site alpha0.4.4.4

---
+++ js/fortuna.js
@@ -0,0 +1,7 @@
+const closeBtn = document.getElementById("modalOverlay");
+
+const modal = document.getElementById("modalOverlay");
+
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none"; });
+
