162cf92b256adba9dbb0e11ed681072d3f11ff08
Dark-Lord777 <207839762+Dark-Lord777@users.noreply.github.com>
2026-03-31 22:26:30 +0300

    fix many small problems/ The first problem which was solved its comments for russian(i rename in to  english lang/ because this look professionally.)/ Solve problem related effectscroll.js/ And some trash was cleared out. And i transfer this commit on new branch archieve

--- js/fortuna.js
+++
@@ -1,7 +0,0 @@
-const closeBtn = document.getElementById("modalOverlay");
-
-const modal = document.getElementById("modalOverlay");
-
-closeBtn.addEventListener("click", function() {
-  modal.style.display = "none"; });
-
